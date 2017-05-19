6216140bc1e999f74606859b005047830da378c5
ext-zsoldosb <Balazs.Zsoldos@knorr-bremse.com>
2017-05-19 13:12:15 +0200

    API improvement

--- src/main/java/org/everit/web/weblet/WebletURIGenerator.java
+++ src/main/java/org/everit/web/weblet/WebletURIGenerator.java
@@ -2,8 +2,11 @@ package org.everit.web.weblet;
 
 import java.net.URI;
 import java.util.Map;
+import java.util.Set;
 
 public interface WebletURIGenerator {
+
+  URI createActionURI(Weblet weblet, Map<String, String> parameters, Set<String> stateIds);
 
   /**
    * Creates a bookmarkable URI that points to the page that holds the context with parameters to
@@ -13,18 +16,16 @@ public interface WebletURIGenerator {
    *          The parameters addressed to the specific weblets.
    * @return An absolute URI starting with path (not containing scheme, host and port).
    */
-  URI createBookmarkableURI(Map<Weblet, Map<String, String>> parametersByWeblets);
+  URI createBookmarkableURI(Weblet weblet, Map<String, String> parameters, Set<String> stateIds);
 
   /**
    * Creates an URI that if used, the {@link Weblet#serveResource(WebletRequest, WebletResponse)}
    * function of the specified {@link Weblet} will be called directly.
    *
-   * @param weblet
-   *          The weblet
    * @param parameters
    * @return An absolute or relative URI starting with path (not containing scheme, host and port).
    *         The URI should work within the current page.
    */
-  URI createResourceURI(Weblet weblet, Map<String, String> parameters);
+  URI createResourceURI(Weblet weblet, Map<String, String> parameters, Set<String> stateIds);
 
 }
